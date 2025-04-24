2d1ae0e6edf111efe49c3925a2b0d045a13b1224
Muhammad Firdaus salleh <firdaus@Muhammads-Laptop.local>
2025-04-24 21:35:49 +0800

    linux vm provision tf configuration

--- linux-vm-provisioning/main.tf
+++ linux-vm-provisioning/main.tf
@@ -1,1 +1,92 @@
 # main.tf content placeholder
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.26.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  # Configuration options
+}
+
+
+
+resource "azurerm_resource_group" "rg1" {
+  name     = "rg1"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "vnet1"
+  address_space       = ["10.0.0/16"]
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+  subnet {
+    name           = "subnet1"
+    address_prefixes = ["10.0.1/24"]
+  }
+}
+resource "azurerm_network_security_group" "nsg1" {
+  name                = "nsg1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  security_rule {
+    name                       = "allow-ssh"
+    priority                   = 1000
+    direction                  = "Inbound"
+    access                    = "Allow"
+    protocol                  = "Tcp"
+    source_port_range         = "*"
+    destination_port_range     = 22
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "tls_private_key" "linux_ssh_key" {
+  # Generate a new SSH key pair
+    algorithm = "RSA"
+    rsa_bits = 4096
+}
+
+resource "azurerm_network_interface" "nic1" {
+  name                = "nic1"
+  location            = azurerm_resource_group.rg1.location
+  resource_group_name = azurerm_resource_group.rg1.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                    = azurerm_virtual_network.vnet1.subnet[0].id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "vm1" {
+    name                = "vm1"
+    location            = azurerm_resource_group.rg1.location
+    resource_group_name = azurerm_resource_group.rg1.name
+    size                = "Standard_F2"
+    admin_username      = "adminuser"
+    network_interface_ids = [azurerm_network_interface.nic1.nice1.id]
+    admin_ssh_key {
+        username = "adminuser"
+        public_key = tls_private_key.linux_ssh_key.public_key_openssh
+    }
+
+    os_disk {
+        caching              = "ReadWrite"
+        storage_account_type = "StandardSSD_LRS"
+        disk_size_gb        = 30
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "22_04-LTS"
+        version   = "latest"
+    }
+}
