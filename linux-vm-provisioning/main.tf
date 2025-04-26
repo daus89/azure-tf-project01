6e08f436bfc3c5d05a48b8ae95cc62e8c2523458
Muhammad Firdaus salleh <firdaus@Muhammads-Laptop.local>
2025-04-26 23:52:23 +0800

    new linux configuration

--- linux-vm-provisioning/main.tf
+++ linux-vm-provisioning/main.tf
@@ -21,14 +21,18 @@ resource "azurerm_resource_group" "rg1" {
 
 resource "azurerm_virtual_network" "vnet1" {
   name                = "vnet1"
-  address_space       = ["10.0.0/16"]
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg1.location
   resource_group_name = azurerm_resource_group.rg1.name
-  subnet {
-    name             = "subnet1"
-    address_prefixes = ["10.0.1/24"]
-  }
 }
+
+resource "azurerm_subnet" "subnet1" {
+  name                 = "subnet1"
+  resource_group_name  = azurerm_resource_group.rg1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
 resource "azurerm_network_security_group" "nsg1" {
   name                = "nsg1"
   location            = azurerm_resource_group.rg1.location
@@ -60,9 +64,14 @@ resource "azurerm_network_interface" "nic1" {
 
   ip_configuration {
     name                          = "ipconfig1"
-    subnet_id                     = azurerm_virtual_network.vnet1.subnet.id
+    subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
   }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg-associate" {
+  subnet_id                 = azurerm_subnet.subnet1.id
+  network_security_group_id = azurerm_network_security_group.nsg1.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
