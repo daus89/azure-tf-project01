239c231a6a0214940002f71538916e4769096412
Muhammad Firdaus salleh <firdaus@Muhammads-Laptop.local>
2025-05-04 17:25:48 +0800

    new storage module

--- three-tier-app/main.tf
+++ three-tier-app/main.tf
@@ -1,1 +1,10 @@
 # main.tf content placeholder
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    environment = "staging"
+    managed-by  = "Terraform"
+  }
+}
--- three-tier-app/variables.tf
+++ three-tier-app/variables.tf
@@ -1,1 +1,10 @@
 # variables.tf content placeholder
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+  
+}
+variable "location" {
+  description = "The Azure region where the resources will be created"
+  type        = string
+}
